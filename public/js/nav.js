41eafa0a291987688e5313d168e8fc9bd9b7a713
salehmmasri <salehalmasri494@yahoo.com>
2020-07-20 13:39:50 +0300

    change-cards

---
+++ public/js/nav.js
@@ -0,0 +1,12 @@
+'use strict';
+var x = document.getElementById("myLinks");
+function myFunction() {
+	if (x.style.display === "inline-block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "inline-block";
+	}
+  }
+
+  
+ 
